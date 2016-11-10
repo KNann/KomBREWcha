01f7286394a3100599c4bd241202c6c8b89a9e2e
Kelsey Nanneman <kcn2@rice.edu>
2016-11-10 01:12:01 -0600

    Alert.ino displays a red LED if the brew is not complete, and a green LED with a sound alert if the brew is complete. Currently, the variable that indicates whether the brew is complete is calculated in colorAnalyze.py and has not yet been imported into this script.

--- Alert/Alert.ino
+++ Alert/Alert.ino
@@ -1,8 +1,24 @@
 #include <Adafruit_CircuitPlayground.h>
 
+int complete=1;
+
 void setup() {
   // put your setup code here, to run once:
 CircuitPlayground.begin();
+
+if (complete) {
+CircuitPlayground.setPixelColor(2,0,200,0);
+CircuitPlayground.playTone(650, 500);
+delay(750);
+CircuitPlayground.playTone(650, 500);
+delay(750);
+CircuitPlayground.playTone(650, 500);
+delay(750);
+delay(2000);}
+else {
+CircuitPlayground.setPixelColor(2,200,0,0);
+delay(2000);}
+CircuitPlayground.clearPixels();
 }
 
 void loop() {
